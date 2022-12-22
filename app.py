ded822aa07a2937dc8fce5119524c6fe2f5b8bef
Avra <96898985+avrabyt@users.noreply.github.com>
2022-12-22 23:18:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pyChatGPT import ChatGPT
 
 session_token = st.secrets["pass"]
-api = ChatGPT(session_token, proxy = 'https://hellochatgpt.streamlit.app')  # auth with session token
+api = ChatGPT(session_token,verbose=True)  # auth with session token
 msg = st.text_area("Prompt:")
 if st.button("Enter"): 
     resp = api.send_message(msg)
